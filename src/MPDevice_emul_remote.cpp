dde70d9af7e47d136a32477f981da2f367bafd8f
Stanislav Bobykin <sbobykin@gmail.com>
2018-02-10 22:45:05 +0500

    remote emu. answers 'Unimplemented emulation command' for all cmds

--- src/MPDevice_emul_remote.cpp
+++ src/MPDevice_emul_remote.cpp
@@ -1,16 +1,24 @@
 #include "MPDevice_emul_remote.h"
+#include "MooltipassCmds.h"
 
 MPDevice_emul_remote::MPDevice_emul_remote(QObject *parent):
     MPDevice(parent)
 {
     qDebug() << "Emulation Device in the remote mode";
-
-    qDebug() << "remote emul WIP, I am exiting :), lol";
-    qApp->exit();
 }
 
 void MPDevice_emul_remote::platformWrite(const QByteArray &data)
 {
+    qDebug() << "Unimplemented emulation command: " << MPCmd::printCmd(data) << data.mid(2);
+
+    QByteArray d = data;
+    d.resize(64);
+    d[2] = 0; //result is 0
+
+    QTimer::singleShot(0, [=]()
+    {
+        emit platformDataRead(data);
+    });
 }
 
 void MPDevice_emul_remote::platformRead()
