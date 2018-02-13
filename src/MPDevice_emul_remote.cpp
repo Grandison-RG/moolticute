e7d6dc6e8acd85943d39051270767b0e3262b2d6
Stanislav Bobykin <sbobykin@gmail.com>
2018-02-13 14:35:56 +0500

    ws wip: sending of hello to the remote emu

--- src/MPDevice_emul_remote.cpp
+++ src/MPDevice_emul_remote.cpp
@@ -5,11 +5,15 @@ MPDevice_emul_remote::MPDevice_emul_remote(QObject *parent):
     MPDevice(parent)
 {
     qDebug() << "Emulation Device in the remote mode";
+    connect(&ws, &QWebSocket::connected, this, &MPDevice_emul_remote::onConnected);
+
+    qDebug() << "Opening of ws connection with a remote emu";
+    ws.open(QUrl(QStringLiteral("ws://localhost:30036")));
 }
 
 void MPDevice_emul_remote::platformWrite(const QByteArray &data)
 {
-    qDebug() << "Unimplemented emulation command: " << MPCmd::printCmd(data) << data.mid(2);
+    //qDebug() << "Unimplemented emulation command: " << MPCmd::printCmd(data) << data.mid(2);
 
     QByteArray d = data;
     d.resize(64);
@@ -21,7 +25,21 @@ void MPDevice_emul_remote::platformWrite(const QByteArray &data)
     });
 }
 
+void MPDevice_emul_remote::onConnected()
+{
+    qDebug() << "Connected to the remote emu";
+
+    ws.sendTextMessage(QStringLiteral("Hello, remote haskell emu!"));
+}
+
 void MPDevice_emul_remote::platformRead()
 {
     qDebug() << "PlatformRead in the remote mode";
+}
+
+MPDevice_emul_remote::~MPDevice_emul_remote()
+{
+
+    qDebug() << "Closing of ws connection with a remote emu";
+    ws.close();
 }
--- src/MPDevice_emul_remote.h
+++ src/MPDevice_emul_remote.h
@@ -1,14 +1,20 @@
 #ifndef MPDEVICE_EMUL_REMOTE_H
 #define MPDEVICE_EMUL_REMOTE_H
+#include <QtWebSockets/QWebSocket>
 #include "MPDevice.h"
 
 class MPDevice_emul_remote : public MPDevice
 {
 public:
-    MPDevice_emul_remote(QObject *parent);
+    explicit MPDevice_emul_remote(QObject *parent);
+    virtual ~MPDevice_emul_remote(); 
 private:
+    QWebSocket ws;
     virtual void platformRead();
     virtual void platformWrite(const QByteArray &data);
+
+private Q_SLOTS:
+    void onConnected();
 };
 
 #endif // MPDEVICE_EMUL_H
